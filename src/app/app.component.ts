46085d799d814a017842a6c1dfee411900b3185b
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-04-18 10:22:30 +0200

    add a menu page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,11 @@ export class AppComponent {
       icon: 'home'
     },
     {
+      title: 'Scan',
+      url: '/scan',
+      icon: 'qr-scanner'
+    },
+    {
       title: 'List',
       url: '/list',
       icon: 'list'
